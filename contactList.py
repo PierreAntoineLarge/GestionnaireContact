b0d969c866465442c46b408bf65a4e0037448154
pa large <pierreantoine.large@gmail.com>
2023-10-26 16:36:24 +0200

    Just one bug to solve

--- contactList.py
+++ contactList.py
@@ -15,9 +15,9 @@ fenetre.geometry("1100x1000")
 selected_contact_number = tk.StringVar()
 
 def add_contact():
-    contact_name = contact_desc_entry.get()
-    contact_lastname = contact_desc_entry.get()
-    contact_number = contact_desc_entry.get()
+    contact_name = name_entry.get()
+    contact_lastname = lastname_entry.get()
+    contact_number = number_entry.get()
 
     lastname_valid = bool(contact_lastname)
     name_valid = bool(contact_name)
@@ -40,7 +40,7 @@ def add_contact():
 def delete_contact():
     selected_index = contact_combobox.current()
     if selected_index >= 0:
-        del contact[selected_index]
+        del contacts[selected_index]
         update_contact_combobox()
 
 def save_contacts():
@@ -62,16 +62,19 @@ def load_contacts():
 def update_selected_contact_number(event=None):
     selected_index = contact_combobox.current()
     if selected_index >= 0:
-        if len(contacts[selected_index]["contact_name"]) > 0:
-          selected_contact_number.set("numéro : " + contacts[selected_index]["contact_number"] + "prénom" + contacts[selected_index]["contact_name"] + "numéro" + contacts[selected_index]["contact_number"])
-        else:          
-          selected_contact_number.set("Aucun numéro")
+        selected_contact = contacts[selected_index]
+        contact_lastname = selected_contact.get("contact_lastname", "N/A")  
+        contact_name = selected_contact.get("contact_name", "N/A")  
+        contact_number = selected_contact.get("contact_number", "N/A") 
+
+        info_text = "Nom: {}\nNuméro de téléphone: {}".format(contact_name, contact_number, contact_lastname)
+        selected_contact_number.set(info_text)
     else:
-        selected_contact_number.set("Aucun contact séléctionné")
+        selected_contact_number.set("Aucun contact sélectionné")
 
 def update_contact_combobox():
-    sorted_contacts = sorted(contacts, key=lambda x: x['contact_name'])  # Triez les contacts par la clé 'contact_name'
-    contact_combobox['values'] = [contact["contact_name"] + contact["contact_number"] for contact in sorted_contacts]
+    sorted_contacts = sorted(contacts, key=lambda x: x['contact_name']) 
+    contact_combobox['values'] = [contact["contact_name"] + " " + contact["contact_lastname"] + " " + contact["contact_number"]  for contact in sorted_contacts]
     contact_combobox.set("")
 
 
@@ -84,6 +87,7 @@ def edit_contact():
         if edited_name and edited_number:
             contacts[selected_index]["contact_name"] = edited_name
             contacts[selected_index]["contact_number"] = edited_number
+            contacts[selected_index]["contact_lastname"] = edited_lastname
             update_contact_combobox()
             messagebox.showinfo("Modification", "Le contact a été modifié avec succès.")
         else:
@@ -95,9 +99,9 @@ def update_selected_contact_number(event=None):
     selected_index = contact_combobox.current()
     if selected_index >= 0:
         selected_contact = contacts[selected_index]
-        selected_contact_number.set("Numéro : " + selected_contact["contact_name"])
-
-        # Affiche les valeurs actuelles dans les champs de modification
+        selected_contact_number.set("Nom : " + selected_contact["contact_name"] + " " + "Prénom :" + selected_contact["contact_lastname"] + " " + " Numéro : " + selected_contact["contact_number"]  )
+        edited_name_entry.delete(0, tk.END)
+        edited_name_entry.insert(0, selected_contact["contact_lastname"])
         edited_name_entry.delete(0, tk.END)
         edited_name_entry.insert(0, selected_contact["contact_name"])
         edited_number_entry.delete(0, tk.END)
@@ -112,13 +116,10 @@ add_label = tk.Label(fenetre, text="Sélectionner un contact",font=55)
 add_label.place(x=350,y=0)
 
 add_label = tk.Label(fenetre, text="Ajoutez un contact",font=55)
-add_label.place(x=350,y=120)
+add_label.place(x=350,y=100)
 
 add_label = tk.Label(fenetre, text="Modifier un contact",font=55)
-add_label.place(x=350,y=370)
-
-contact_name_label = tk.Label(fenetre, text="Nom du contact:", font=35)
-contact_name_label.place(x=40,y=150)
+add_label.place(x=350,y=350)
 
 contact_combobox = Combobox(fenetre, width=50)
 contact_combobox.place(x=100,y=50)
@@ -133,17 +134,26 @@ selected_contact_label.place(x=550,y=50)
 delete_button = tk.Button(fenetre, text="Supprimer", command=delete_contact, font=35)
 delete_button.place(x=750,y=40)
 
-contact_number_label = tk.Label(fenetre, text="son numéro:", font=35)
-contact_number_label.place(x=50,y=200)
+contact_name_label = tk.Label(fenetre, text="Prénom:", font=35)
+contact_name_label.place(x=40,y=130)
 
-detail_desc_entry = tk.Entry(fenetre, width=60)
-detail_desc_entry.place(x=200,y=150)
+contact_lastname_label = tk.Label(fenetre, text="Nom :", font=35)
+contact_lastname_label.place(x=40,y=180)
+
+contact_number_label = tk.Label(fenetre, text="Numéro:", font=35)
+contact_number_label.place(x=40,y=220)
+
+name_entry = tk.Entry(fenetre, width=60)
+name_entry.place(x=200,y=180)
+
+lastname_entry = tk.Entry(fenetre, width=60)
+lastname_entry.place(x=200,y=135)
+
+number_entry = tk.Entry(fenetre, width=60)
+number_entry.place(x=200,y=220)
 
 add_button = tk.Button(fenetre, text="Ajouter le contact", command=add_contact, font=35)
-add_button.place(x=250,y=250)
-
-contact_desc_entry = tk.Entry(fenetre, width=60)
-contact_desc_entry.place(x=200,y=200)
+add_button.place(x=620,y=170)
 
 load_button = tk.Button(fenetre, text="Charger une liste de contacts", command=load_contacts, font=35)
 load_button.place(x=50,y=600)
@@ -151,22 +161,26 @@ load_button.place(x=50,y=600)
 save_button = tk.Button(fenetre, text="Enregistrer les contacts dans un ficher", command=save_contacts, font=35)
 save_button.place(x=350,y=600)
 
-
-
 edited_name_label = tk.Label(fenetre, text="Nouveau nom:", font=("Arial", 14))
-edited_name_label.place(x=50, y=400)
+edited_name_label.place(x=40, y=380)
 
-edited_name_entry = tk.Entry(fenetre, width=40)
-edited_name_entry.place(x=200, y=400)
+edited_lastname_label = tk.Label(fenetre, text="Nouveau prénom:", font=("Arial", 14))
+edited_lastname_label.place(x=40, y=420)
 
 edited_number_label = tk.Label(fenetre, text="Nouveau numéro:", font=("Arial", 14))
-edited_number_label.place(x=50, y=450)
+edited_number_label.place(x=40, y=450)
+
+edited_name_entry = tk.Entry(fenetre, width=40)
+edited_name_entry.place(x=200, y=390)
+
+edited_lastname_entry = tk.Entry(fenetre, width=40)
+edited_lastname_entry.place(x=200, y=420)
 
 edited_number_entry = tk.Entry(fenetre, width=40)
 edited_number_entry.place(x=200, y=450)
 
 edit_button = tk.Button(fenetre, text="Modifier", command=edit_contact, font=("Arial", 14))
-edit_button.place(x=250, y=500)
+edit_button.place(x=620, y=450)
 
 
 
